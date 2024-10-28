76009a4ead0652003c69f6cf55eef383c78d283f
Spencer Ye <spencerye425@gmail.com>
2024-10-28 15:59:20 -0400

    fix types based on article

--- src/lib/types.ml
+++ src/lib/types.ml
@@ -1,7 +1,18 @@
 module type simulation =
   sig
+  	(* Simulation Type *)
+  	type t 
+
+  	(* Can change based on what the simulation needs *)
     type action = int
     type response = { observation : int list; reward : int; terminated : bool; truncated : bool; info : string }
-    val reset : int -> unit
-    val step : action -> response
+
+    (* Creates a new simulation *)
+    val create : unit -> t 
+
+    (* Resets the simulation and returns the first response again *)
+    val reset : t -> response
+
+    (* Applies the action to the environment, and returns the corresponding response *)
+    val step : t -> action -> response
   end
