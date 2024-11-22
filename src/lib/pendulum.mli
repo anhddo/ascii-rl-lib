e103d20b11d6d6c5cb7a9e63a309656165dafc48
Jishuo Yang <101232763+Yjs2666@users.noreply.github.com>
2024-11-22 13:32:21 -0500

    Update pendulum.mli

--- src/lib/pendulum.mli
+++ src/lib/pendulum.mli
@@ -1,15 +1,22 @@
-
 (* Pendulum is a Simulation *)
-include Simulation.T
 
-(* Expose Helper Functions for Testing *)
+type t = float list (* [angle; angular_speed] *)
+type action = float list (* [applied_torque] *)
+
+type response = {
+  observation : t;
+  reward : float;
+  terminated : bool;
+  truncated : bool;
+  info : string;
+}
 
 val normalize_angle : float -> float
-
-val modulo : float -> float -> float 
-
+val modulo : float -> float -> float
 val square : float -> float
-
-val random_between : float -> float -> float 
-
+val random_between : float -> float -> float
 val clip : 'a -> 'a -> 'a -> 'a
+val create : unit -> t
+val reset : unit -> t
+val render : t -> unit
+val simulate : t -> unit
