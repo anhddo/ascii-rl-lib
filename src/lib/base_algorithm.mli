3b95098a812d18cde49cd4a63dcf3ff36eeb2508
Anh <>
2024-11-24 13:46:26 -0500

    update

--- src/lib/base_algorithm.mli
+++ src/lib/base_algorithm.mli
@@ -3,3 +3,12 @@ module type S = sig
   (* val train : Pytypes.pyobject -> int -> unit *)
   val choose_action : float list -> float list
 end
+
+(* module type QLearning_config = sig
+  val alpha : float
+  val gamma : float
+  val epsilon : float
+  val num_episodes : int
+  val max_steps : int
+
+end *)
