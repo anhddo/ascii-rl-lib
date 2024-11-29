0a7be360a58296d78c661bddba4d4b20365d793e
Anh Do <anhddo93@gmail.com>
2024-11-29 18:45:00 -0500

    fix q learning out of bin bug

--- test/state_action_test.ml
+++ test/state_action_test.ml
@@ -20,12 +20,16 @@ let test_func x y =
   assert_equal x y ~msg:Printf.(sprintf "Expected %d, got %d" x y)
 
 let tests =
-  "Cartpole test"
+  "State_action tests"
   >::: [
          ( "bin test" >:: fun _ ->
            assert_equal 1 (Cartpole_state_action.value_to_bin 0.7 0. 1. 2) );
+         ( "bin test1.1" >:: fun _ ->
+           test_func 0 (Cartpole_state_action.value_to_bin 0.0 0. 1. 2) );
+         ( "bin test1.2" >:: fun _ ->
+           test_func 1 (Cartpole_state_action.value_to_bin 1.0 0. 1. 2) );
          ( "bin test1" >:: fun _ ->
-           assert_equal 0 (Cartpole_state_action.value_to_bin 0.01 0. 1. 2) );
+           test_func 0 (Cartpole_state_action.value_to_bin 0.01 0. 1. 2) );
          ( "bin test2" >:: fun _ ->
            assert_equal 3 (Cartpole_state_action.value_to_bin 0.75 0. 1. 4) );
          ( "bin test3" >:: fun _ ->
@@ -54,9 +58,22 @@ let tests =
            test_func 3 (Pendulum_state_action.value_to_bin 1. (-1.) 1. 4) );
          ( "bin test pendulum2 " >:: fun _ ->
            test_func 0 (Pendulum_state_action.value_to_bin (-1.) (-1.) 1. 4) );
+         ( "bin test pendulum3 " >:: fun _ ->
+           test_func 19 (Pendulum_state_action.value_to_bin 1. (-1.) 1. 20) );
          ( "pendulum convert" >:: fun _ ->
            test_func 1
              (Pendulum_state_action.convert_state_to_bin [ -1.; -1.; -7.19 ]) );
+         ( "pendulum convert1" >:: fun _ ->
+          let v = Pendulum_state_action.convert_state_to_bin [1.; -1.25208246222e-008; 0.314987382668] in
+           assert_bool "test" 
+             (Int.compare 8000  v = 1) );
+         ( "pendulum convert2" >:: fun _ ->
+          let v = Pendulum_state_action.convert_state_to_bin [0.999993687422; -0.00355318389298; 0.152762888634] in
+           assert_equal 7790 v);
+         ( "pendulum convert3" >:: fun _ ->
+          let v = Pendulum_state_action.convert_state_to_bin [0.999896233083; -0.01440566092; -0.0835999333127] in
+          print_int v;
+           assert_equal 7789 v)
        ]
 
 let () = run_test_tt_main tests
