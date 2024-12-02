6a125eeef5c96b836d819aec3310eab4f1eae697
ye-spencer <spencerye@Spencers-PC>
2024-12-02 14:43:38 -0500

    clear up cartpole to prepare for submission

--- src/lib/cartpole.mli
+++ src/lib/cartpole.mli
@@ -1,10 +1,4 @@
 module Make (_ : Simulation.Config) : sig
   include Simulation.S
 
-  val normalize_angle : float -> float
-  val modulo : float -> float -> float
-  val square : float -> float
-  val random_between : float -> float -> float
-  val clip : 'a -> 'a -> 'a -> 'a
-  val simulate : t -> unit
 end
