9500f0763f07fc4bc84092f110863893a454a7f4
y-aang <2386139091@qq.com>
2024-12-02 05:41:15 -0500

    add: valid script for vpg and vpg_nn

--- src/lib/vpg.ml
+++ src/lib/vpg.ml
@@ -1,17 +1,16 @@
-(* A demo for vpg algorithm 
-  - the reward is time steps length, which is currently wrong *)
+(* a backup version for vpg without nn*)
 [@@@ocaml.warning "-27"]
 [@@@ocaml.warning "-26"]
 [@@@ocaml.warning "-33"]
 
-let () = Py.initialize ()
+let () = Py.initialize () ~interpreter:"/Users/shenyang/opt/anaconda3/envs/pytorch/bin/python"
 
 let gym = Py.import "gymnasium"
 
-let env_render =
+(* let env_render =
   Py.Module.get_function_with_keywords gym "make"
     [| Py.String.of_string "CartPole-v1" |]
-    [ ("render_mode", Py.String.of_string "human") ]
+    [ ("render_mode", Py.String.of_string "human") ] *)
 
 let env =
   Py.Module.get_function gym "make" [| Py.String.of_string "CartPole-v1" |]
@@ -75,14 +74,11 @@ let convert_state_to_bin (state : float list) : int =
   in
   convert_state_to_bin' state_bin 3
 
-(* Parameter for VPG *)
 let num_states = int_of_float @@ Float.pow 20.0 4.0
 let num_actions = 2
 
-(* Init the policy parameters *)
 let policy_params = Array.make_matrix num_states num_actions 0.0
 
-(* Softmax *)
 let softmax arr =
   let max_elem = Array.fold_left max neg_infinity arr in
   let exps = Array.map (fun x -> exp (x -. max_elem)) arr in
@@ -97,7 +93,7 @@ let select_action state =
     cumulative_probs.(i) <- cumulative_probs.(i - 1) +. probs.(i)
   done;
   let r = Random.float 1.0 in
-  let rec find_action i =
+  let rec find_action i = 
     if i >= Array.length cumulative_probs then
       Array.length cumulative_probs - 1
     else if r <= cumulative_probs.(i) then i
@@ -105,7 +101,6 @@ let select_action state =
   in
   find_action 0
 
-(* Update the policy of VPG parameters *)
 let update_policy trajectories learning_rate =
   let returns = Array.of_list (List.map snd trajectories) in
   let states_actions = Array.of_list (List.map fst trajectories) in
@@ -123,44 +118,60 @@ let update_policy trajectories learning_rate =
     done
   done
   
+let calculate_returns rewards gamma =
+  let rec aux acc returns = function
+    | [] -> List.rev returns
+    | r :: rs ->
+        let g_t = r +. gamma *. acc in
+        aux g_t (g_t :: returns) rs
+  in
+  aux 0.0 [] (List.rev rewards)
 
+(* let update_trajectories trajectories rewards gamma =
+  let returns = calculate_returns rewards gamma in
+  List.map2 (fun (s_a, _) g_t -> (s_a, g_t)) trajectories returns *)
+
+let update_trajectories trajectories rewards gamma =
+  let returns = calculate_returns rewards gamma in
+  (* 标准化 returns *)
+  let mean = List.fold_left (+.) 0.0 returns /. float_of_int (List.length returns) in
+  let variance = List.fold_left (fun acc x -> acc +. (x -. mean) ** 2.0) 0.0 returns /. float_of_int (List.length returns) in
+  let std_dev = sqrt (variance +. 1e-8) in
+  let standardized_returns = List.map (fun r -> (r -. mean) /. std_dev) returns in
+  List.map2 (fun (s_a, _) g_t -> (s_a, g_t)) trajectories standardized_returns
+  
 let loop env =
   let num_episodes = 1000 in  (*1000*)
-  let max_steps = 3000000 in    (*200*)
+  let max_steps = 200 in    (*200*)
   let learning_rate = 0.05 in
+  let gamma = 0.99 in
   for episode = 1 to num_episodes do
     let state = reset_fn env in
     let state_bin = convert_state_to_bin state in
-    let rec run_step t state_bin trajectories =
+    let rec run_step t state_bin trajectories rewards =
       if t >= max_steps then
-        (* Update the policy *)
-        let reward = List.length trajectories |> float_of_int in
-        let trajectories =
-          List.map (fun (s_a, _) -> (s_a, reward)) trajectories
-        in
-        update_policy trajectories learning_rate;
-        Printf.printf "Episode %d: Total Reward: %f\n%!" episode reward
+        let total_reward = List.fold_left (+.) 0.0 rewards in
+        Printf.printf "Episode %d Success: Total Reward: %f\n%!" episode total_reward
       else
         let action = select_action state_bin in
-        let next_state, _, is_done, _, _ = step_fn env action in
+        (* Printf.printf "Selected action: %d\n%!" action; *)
+        let next_state, reward, is_done, _, _ = step_fn env action in
         let next_state_bin = convert_state_to_bin next_state in
         let trajectories = ((state_bin, action), 0.0) :: trajectories in
+        let rewards = reward :: rewards in
+        (* Printf.printf "length trajectories %d\n rewards: %d\n" (List.length trajectories) (List.length rewards); *)
         if is_done then
-          (* Update the policy *)
-          let reward = List.length trajectories |> float_of_int in
-          let trajectories =
-            List.map (fun (s_a, _) -> (s_a, reward)) trajectories
-          in
-          update_policy trajectories learning_rate;
-          Printf.printf "Episode %d: Total Reward: %f\n%!" episode reward
+          let updated_trajectories = update_trajectories trajectories rewards gamma in
+          update_policy updated_trajectories learning_rate;
+          let total_reward = List.fold_left (+.) 0.0 rewards in
+          Printf.printf "Episode %d: Total Reward: %f\n%!" episode total_reward
         else
-          run_step (t + 1) next_state_bin trajectories
+          run_step (t + 1) next_state_bin trajectories rewards
     in
-    run_step 0 state_bin []
+    run_step 0 state_bin [] []
   done
+  
 
-
-(* A demo for calling the vpg algorithm in 'cartpole' *)
-(* let () =
+let () =
   loop env;
-  loop env_render; *)
+  (* loop env_render *)
