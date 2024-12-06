54a8af476e75a5eb89914d0970e61a51592b5c29
Anh Do <anhddo93@gmail.com>
2024-12-05 22:03:06 -0500

    make main.ml tidy

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,48 +1,40 @@
 (* Variables to store the parsed values *)
 let episode = ref 0
 let model_path = ref ""
-
 let render = ref false
 
 (* Function to set the episode *)
 
 (* Command-line argument specification *)
-let speclist = [
-  ("--episode", Arg.Set_int episode, "Set the number of episodes (integer)");
-  ("--model-path", Arg.Set_string model_path, "Set the path to the model (string)");
-  ("--render", Arg.Set render, "Enable rendering (boolean flag, default: false)");
-]
+let speclist =
+  [
+    ("--episode", Arg.Set_int episode, "Set the number of episodes (integer)");
+    ( "--model-path",
+      Arg.Set_string model_path,
+      "Set the path to the model (string)" );
+    ( "--render",
+      Arg.Set render,
+      "Enable rendering (boolean flag, default: false)" );
+    (*We will include the following argument after we integrate other simulations and algorithms
+      "--algo", Arg.Set algo, "algorithm name" );
+      "--simulation", Arg.Set Simulation, "algorithm name" ); *)
+  ]
 
 (* Main program *)
-let run (episode: int) (model_path: string) (render: bool) = 
-
+let run (episode : int) (model_path : string) (render : bool) =
   let module Algo_config = struct
     let model_path = model_path
   end in
-
   let module Pendulum_env = Pendulum.Make (struct
-    let render = false
+    let render = render
   end) in
-
-  let module Pendulum_env_render = Pendulum.Make (struct
-    let render = true
-  end) in
-
   let module Qlearning_algo = Qlearning.Make (Algo_config) (Pendulum_env) in
-
-  let module Qlearning_algo_render =
-    Qlearning.Make (Algo_config) (Pendulum_env_render) in
-  if render then
-    Qlearning_algo_render.train episode
-  else
-  (Qlearning_algo.train episode;
-  Qlearning_algo.save_q_table ();)
-
-  
+  Qlearning_algo.train episode;
+  Qlearning_algo.save_model ()
 
 (* Entry point *)
-let () = 
+let () =
   Arg.parse speclist print_endline "";
 
   (* Validate required arguments *)
-  run !episode !model_path !render;
+  run !episode !model_path !render
