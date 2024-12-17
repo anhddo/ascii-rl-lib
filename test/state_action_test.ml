a135006847effdc62e70eeefb9113fb9e184b419
Anh Do <anhddo93@gmail.com>
2024-12-16 22:42:07 -0500

    add bin_to_value test

--- test/state_action_test.ml
+++ test/state_action_test.ml
@@ -1,21 +1,16 @@
 open OUnit2
 (* we usually open OUnit2 since it is pervasively used in test files *)
 
-
 module Config = struct
   let simulation_name = ""
   let render = false
 end
+
 module Cartpole_env = Cartpole.Make (Config)
 module Pendulum_env = Pendulum.Make (Config)
 module Cartpole_state_action = State_action.Make (Cartpole_env)
+module Pendulum_state_action = State_action.Make (Pendulum_env)
 
-module Pendulum_state_action  = State_action.Make (Pendulum_env)
-
-
-(* next_state: (1. -1.45140206468e-008 -0.120906035639)
-next_state_bin: 8189
-Fatal error: exception Invalid_argument("index out of bounds") *)
 let test_func x y =
   (* Printf.printf  *)
   assert_equal x y ~msg:Printf.(sprintf "Expected %d, got %d" x y)
@@ -34,11 +29,13 @@ let tests =
          ( "bin test2" >:: fun _ ->
            assert_equal 3 (Cartpole_state_action.value_to_bin 0.75 0. 1. 4) );
          ( "bin test3" >:: fun _ ->
-           assert_equal 0 (Cartpole_state_action.value_to_bin (-0.75) 0. 1. 4) );
+           assert_equal 0 (Cartpole_state_action.value_to_bin (-0.75) 0. 1. 4)
+         );
          ( "bin test4" >:: fun _ ->
            assert_equal 3 (Cartpole_state_action.value_to_bin 1.75 0. 1. 4) );
          ( "bin test5" >:: fun _ ->
-           assert_equal 1 (Cartpole_state_action.value_to_bin (-3.59) (-4.) 4. 20) );
+           assert_equal 1
+             (Cartpole_state_action.value_to_bin (-3.59) (-4.) 4. 20) );
          ( "bin test6" >:: fun _ ->
            assert_equal 1 (Cartpole_state_action.value_to_bin 0.25 0. 1. 4) );
          ( "convert" >:: fun _ ->
@@ -50,31 +47,48 @@ let tests =
              (Cartpole_state_action.convert_state_to_bin
                 [ -4.8; -3.59; -0.418; -3.59 ]) );
          ( "bin test pendulum" >:: fun _ ->
-           test_func 1 (Pendulum_state_action.value_to_bin (-7.19) (-8.) 8. 20) );
+           test_func 1 (Pendulum_state_action.value_to_bin (-7.19) (-8.) 8. 20)
+         );
          ( "bin test pendulum" >:: fun _ ->
-           test_func 19 (Pendulum_state_action.value_to_bin (8.3) (-8.) 8. 20) );
+           test_func 19 (Pendulum_state_action.value_to_bin 8.3 (-8.) 8. 20) );
          ( "bin test pendulum" >:: fun _ ->
-           test_func 0 (Pendulum_state_action.value_to_bin (-8.3) (-8.) 8. 20) );
+           test_func 0 (Pendulum_state_action.value_to_bin (-8.3) (-8.) 8. 20)
+         );
          ( "bin test pendulum1 " >:: fun _ ->
            test_func 3 (Pendulum_state_action.value_to_bin 1. (-1.) 1. 4) );
          ( "bin test pendulum2 " >:: fun _ ->
            test_func 0 (Pendulum_state_action.value_to_bin (-1.) (-1.) 1. 4) );
          ( "bin test pendulum3 " >:: fun _ ->
            test_func 19 (Pendulum_state_action.value_to_bin 1. (-1.) 1. 20) );
+         ( "bin to value test" >:: fun _ ->
+           let value =
+             Pendulum_state_action.bin_to_value 19
+               { low = 0.0; high = 1.0; num_bins = 10 }
+           in
+           assert_bool "value is close to 1.9"
+             (abs_float (1.9 -. value) < 0.0001) );
          ( "pendulum convert" >:: fun _ ->
            test_func 1
              (Pendulum_state_action.convert_state_to_bin [ -1.; -1.; -7.19 ]) );
          ( "pendulum convert1" >:: fun _ ->
-          let v = Pendulum_state_action.convert_state_to_bin [1.; -1.25208246222e-008; 0.314987382668] in
-           assert_bool "test" 
-             (Int.compare 8000  v = 1) );
+           let v =
+             Pendulum_state_action.convert_state_to_bin
+               [ 1.; -1.25208246222e-008; 0.314987382668 ]
+           in
+           assert_bool "test" (Int.compare 8000 v = 1) );
          ( "pendulum convert2" >:: fun _ ->
-          let v = Pendulum_state_action.convert_state_to_bin [0.999993687422; -0.00355318389298; 0.152762888634] in
-           assert_equal 7790 v);
+           let v =
+             Pendulum_state_action.convert_state_to_bin
+               [ 0.999993687422; -0.00355318389298; 0.152762888634 ]
+           in
+           assert_equal 7790 v );
          ( "pendulum convert3" >:: fun _ ->
-          let v = Pendulum_state_action.convert_state_to_bin [0.999896233083; -0.01440566092; -0.0835999333127] in
-          print_int v;
-           assert_equal 7789 v)
+           let v =
+             Pendulum_state_action.convert_state_to_bin
+               [ 0.999896233083; -0.01440566092; -0.0835999333127 ]
+           in
+           print_int v;
+           assert_equal 7789 v );
        ]
 
 let () = run_test_tt_main tests
